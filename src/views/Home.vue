1aeeb4a4115e66ef13e2704edd5247741136ba62
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-08-21 15:32:55 +0700

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,9 +13,13 @@
             }
         },
         created() {
-            fetch(process.env.VUE_APP_BASE_PATH + '/md' + this.$route.path + '.md').then(response => {
+            const md = this.$route.path === '/' ? '/index' : this.$route.path
+            const basePath = process.env.VUE_APP_BASE_PATH
+            fetch(basePath + '/md' + md + '.md').then(response => {
                 response.text().then(text => {
                     this.md = marked(text)
+                        .replace(new RegExp('(src|href)="(../)*_images/', 'g'), '$1="' + basePath + '/md/_images/')
+                        .replace(new RegExp('(src|href)="(?<!http)(../)*(.+)\\.md', 'g'), '$1="' + basePath + '/$3')
                 })
             }).catch(error => {
                 console.log(error)
